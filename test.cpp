0ae6064d39a08fc84f0d707810481391eee0d6b3
xSerejechka <100136969+xSerejechka@users.noreply.github.com>
2022-06-05 13:14:13 +0300

    Lab03 done

--- test.cpp
+++ test.cpp
@@ -1,19 +1,54 @@
 #include "histogram.h"
-#include <vector>
 #include <cassert>
-using namespace std;
 
-void
-test_positive() {
-    vector <double> test = {1.0, 2.0, 3.0};
+void test_positive() {
     double min = 0;
     double max = 0;
-    find_minmax(test, min, max);
-    assert(min == 1.0);
-    assert(max == 3.0);
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
 }
 
-int
-main() {
+void test_negative(){
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+    return;
+}
+
+void test_equal(){
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+    return;
+}
+
+void test_one(){
+    double min = 0;
+    double max = 0;
+    find_minmax({1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+    return;
+}
+
+void test_null(){
+    double min = 5;
+    double max = 7;
+    find_minmax({}, min, max);
+    assert(min == 5);
+    assert(max == 7);
+    return;
+}
+
+int main() {
     test_positive();
+    test_negative();
+    test_equal();
+    test_one();
+    test_null();
 }
