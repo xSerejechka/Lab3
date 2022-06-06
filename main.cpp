aa4484013bece4e8a4495130b026c6d267a92266
xSerejechka <100136969+xSerejechka@users.noreply.github.com>
2022-06-06 12:47:49 +0300

    Lab04 part1 done!

--- main.cpp
+++ main.cpp
@@ -23,16 +23,29 @@ vector<double> input_numbers(istream& in, size_t cnt) {
     return result;
 }
 
+vector<double> rendor_vector(int cnt) {
+    vector<double> hisla(cnt);
+    srand(time(0));
+    for (int i = 0; i < cnt; i++) {
+        hisla[i] = 1 + rand() % 5;
+    }
+    return hisla;
+}
 
-Input read_input(istream& in, bool prompt) {
+Input read_input(istream& in, bool prompt, int cnt = 0) {
     Input data;
-    if (prompt)
-        cerr << "enter number count: ";
-    size_t number_count;
-    in >> number_count;
-    if (prompt)
-        cerr << "Enter numbers: ";
-    data.numbers = input_numbers(in, number_count);
+    if (cnt > 0) {
+        data.numbers = rendor_vector(cnt);
+    }
+    else {
+        if (prompt)
+            cerr << "enter number count: ";
+        size_t number_count;
+        in >> number_count;
+        if (prompt)
+            cerr << "Enter numbers: ";
+        data.numbers = input_numbers(in, number_count);
+    }
     if (prompt)
         cerr << "enter bin count: ";
     in >> data.bin_count;
@@ -45,18 +58,18 @@ vector<size_t> make_histogram(Input input)
     find_minmax(input.numbers, min, max);
     double bin_size = (max - min) / input.bin_count;
     vector<size_t> bins(input.bin_count, 0);
-    for (size_t i = 0; i < input.numbers.size(); i++){
+    for (size_t i = 0; i < input.numbers.size(); i++) {
         bool found = false;
-        for (size_t j = 0; j < input.bin_count - 1 && !found; j++){
+        for (size_t j = 0; j < input.bin_count - 1 && !found; j++) {
             auto lo = min + j * bin_size;
             auto hi = min + (j + 1) * bin_size;
 
-            if (lo <= input.numbers[i] && input.numbers[i] < hi){
+            if (lo <= input.numbers[i] && input.numbers[i] < hi) {
                 bins[j]++;
                 found = true;
             }
         }
-        if (!found){
+        if (!found) {
             bins[input.bin_count - 1]++;
         }
     }
@@ -64,68 +77,68 @@ vector<size_t> make_histogram(Input input)
 }
 
 void show_histogram_text(vector<size_t> bins) {
-	const size_t SCREEN_WIDTH = 80;
-	const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
-	size_t max_count = 0;
-	for (size_t count : bins) {
-		if (count > max_count) {
-			max_count = count;
-		}
-	}
-	const bool scaling_needed = max_count > MAX_ASTERISK;
+    size_t max_count = 0;
+    for (size_t count : bins) {
+        if (count > max_count) {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
 
-	double max_height = max_count;
-	if (max_count > MAX_ASTERISK) {
-		max_height = MAX_ASTERISK;
-	}
+    double max_height = max_count;
+    if (max_count > MAX_ASTERISK) {
+        max_height = MAX_ASTERISK;
+    }
 
-	for (size_t bin : bins) {
-		if (bin < 100) {
-			cout << ' ';
-		}
-		if (bin < 10) {
-			cout << ' ';
-		}
-		cout << bin << "|";
+    for (size_t bin : bins) {
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin << "|";
 
-		size_t height = bin;
-		if (scaling_needed) {
-			const double scaling_factor = (double)MAX_ASTERISK / max_count;
-			height = (size_t)(bin * scaling_factor);
-		}
+        size_t height = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
 
-		for (size_t i = 0; i < height; i++) {
-			cout << '*';
-		}
-		cout << '\n';
-	}
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
 
-	for (size_t bin : bins) {
-		double height = bin;
+    for (size_t bin : bins) {
+        double height = bin;
 
-		if (scaling_needed) {
-			const double scaling_factor = (double)MAX_ASTERISK / max_count;
-			height = (size_t)(bin * scaling_factor);
-		}
-		for (size_t i = 0; i < max_height - height - 1; i++) {
-			cout << ' ';
-		}
-		for (size_t i = 0; i < height; i++) {
-			cout << '*';
-		}
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+        for (size_t i = 0; i < max_height - height - 1; i++) {
+            cout << ' ';
+        }
+        for (size_t i = 0; i < height; i++) {
+            cout << '*';
+        }
 
-		cout << '|';
+        cout << '|';
 
-		if (bin < 100) {
-			cout << ' ';
-		}
-		if (bin < 10) {
-			cout << ' ';
-		}
-		cout << bin;
-		cout << endl;
-	}
+        if (bin < 100) {
+            cout << ' ';
+        }
+        if (bin < 10) {
+            cout << ' ';
+        }
+        cout << bin;
+        cout << endl;
+    }
 }
 
 size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
@@ -137,35 +150,59 @@ size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
 
 Input download(const string& address) {
     stringstream buffer;
-    CURL *curl = curl_easy_init();
-        if(curl) {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-            res = curl_easy_perform(curl);
-            if (res != CURLE_OK) {
-                cerr << curl_easy_strerror(res) << endl;
-                exit(1);
-            }
-            curl_easy_cleanup(curl);
+    CURL* curl = curl_easy_init();
+    if (curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            cerr << curl_easy_strerror(res) << endl;
+            exit(1);
         }
+        curl_easy_cleanup(curl);
+    }
 
     return read_input(buffer, false);
 }
 
 int main(int argc, char* argv[]) {
-	Input data;
-    if (argc > 1) {
-        data = download(argv[1]);
-    } else {
+    Input data;
+    int cnt;
+    bool hislo;
+    if (argc > 1)
+    {
+        string url;
+        for (int i = 1; i < argc; i++) {
+            if (strstr(argv[i], "-generate")) {
+                if (!isdigit(argv[i + 1][0])) {
+                    cerr << "Restart program and enter number count" << endl;
+                    return 0;
+                }
+                else {
+                    cnt = stoi(argv[i + 1]);
+                    hislo = true;
+                }
+            }
+            if (strstr(argv[i], "http://")) {
+                url = argv[i];
+            }
+        }
+        if (hislo) {
+            data = read_input(cin, true, cnt);
+        } else {
+            data = download(url);
+        }
+    }
+    else {
         data = read_input(cin, true);
     }
 
-	//Расчёт Гистограммы
-	const auto bins = make_histogram(input);
+    //Расчёт Гистограммы
+    const auto bins = make_histogram(data);
 
-	//Вывод данных
+    //Вывод данных
     show_histogram_svg(bins);
-	return 0;
+    return 0;
 }
